7a5eaa17ccba62f50c20a5174e62ebf778578c66
asrorovuz <asrorovsardor07@gmail.com>
2025-11-26 14:42:53 +0500

    fix app router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,19 @@
-
-import { RouterProvider } from "react-router-dom";
-import { router } from "./config/routes";
-import { AuthProvider, QueryProvider } from "./providers";
+import { AuthProvider } from "./providers";
 import MessageDispatcher from "@/shared/lib/dispatcher";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppRouter } from "./config/routes";
+import { queryConfig } from "./config/app.config";
 
 function App() {
   const queryClient = new QueryClient(queryConfig);
 
   return (
-
     <MessageDispatcher>
-<QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <AppRouter />
-      </AuthProvider>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <AppRouter />
+        </AuthProvider>
+      </QueryClientProvider>
     </MessageDispatcher>
   );
 }
--- src/shared/lib/dispatcher.ts
+++ src/shared/lib/dispatcher.ts
@@ -1,25 +1,25 @@
-import { useEffect, type ReactNode } from "react"
-import EventBus from "./eventBus"
-import { NOTIFY_NEW_VERSION } from "@/app/constants/version.message"
+import { useEffect, type ReactNode } from "react";
+import EventBus from "./eventBus";
+import { NOTIFY_NEW_VERSION } from "@/app/constants/version.message";
 const MessageDispatcher = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     const handler = ({ name, payload }: any) => {
       if (name === NOTIFY_NEW_VERSION) {
-        EventBus.dispatch(NOTIFY_NEW_VERSION, payload)
+        EventBus.dispatch(NOTIFY_NEW_VERSION, payload);
       }
-    }
+    };
 
     // astilectron mavjud bo‘lsa, onMessage ishlatamiz
     if (window.astilectron?.onMessage) {
-      window.astilectron.onMessage(handler)
+      window.astilectron.onMessage(handler);
     }
 
     return () => {
-      EventBus.remove(NOTIFY_NEW_VERSION, handler) 
-    }
-  }, [])
+      EventBus.remove(NOTIFY_NEW_VERSION, handler);
+    };
+  }, []);
 
-  return children
-}
+  return children;
+};
 
-export default MessageDispatcher
+export default MessageDispatcher;
