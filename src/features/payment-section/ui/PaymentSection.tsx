7867e0e6289a37307d3f160e220ae503db4ba35d
asrorovuz <asrorovsardor07@gmail.com>
2026-05-23 17:41:47 +0500

    fic payment section

--- src/features/payment-section/ui/PaymentSection.tsx
+++ src/features/payment-section/ui/PaymentSection.tsx
@@ -35,7 +35,7 @@ const PaymentSection = ({
     updateDraftDiscount,
     value,
     setValue,
-    setActiveType,
+    // setActiveType,
     setActivePaymentSelectType,
     isBlockSell,
     setIsBlockSell,
